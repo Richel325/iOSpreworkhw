c89e571cb31e01ae45e7084e1d7d40b99df96330
Richel Cuyler <richelcuyler@icloud.com>
2016-10-20 17:51:22 -0400

    iOS Prework HW for Github Assessment

---
+++ 01-task/[richelcuyler] - precourse - app/[richelcuyler] - precourse - app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  [richelcuyler] - precourse - app
+//
+//  Created by Richel Cuyler on 9/20/16.
+//  Copyright © 2016 Richel Cuyler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
